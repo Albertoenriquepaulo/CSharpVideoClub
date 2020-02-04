7f6b7f457a036bf6944ef7e20a08301b807ed1b6
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-02-04 12:39:08 +0100

    initial commit

---
+++ VideoClub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Console = Colorful.Console;
+
+namespace VideoClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
